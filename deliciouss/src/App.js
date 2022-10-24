4974bdafa12840a8f0c862de372a1eda06636405
bilgee0119 <bilgeeen416@gmail.com>
2022-10-24 10:55:40 +0800

    10/24 Lab5

---
+++ deliciouss/src/App.js
@@ -0,0 +1,15 @@
+import Popular from "./components/Popular";
+import Pages from "./pages/Pages";
+import Home from "./pages/Home";
+import Asd from "./pages/asd";
+function App() {
+  return (
+    <div className="App">
+    <h1>Hello</h1>
+     
+     <Pages/>
+    </div>
+  );
+}
+
+export default App;
---
+++ deliciouss/src/pages/asd.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Asd() {
+  return (
+    <div>asd</div>
+  )
+}
+
+export default Asd
