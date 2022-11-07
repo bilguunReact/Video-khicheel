b56573cbebafb57ef96e98300b04ab501efc5650
bilgee0119 <bilgeeen416@gmail.com>
2022-11-07 12:42:38 +0800

    11/07

--- social-app/src/pages/home/Home.jsx
+++ social-app/src/pages/home/Home.jsx
@@ -1,12 +1,42 @@
+import { useState } from "react";
+import { useEffect } from "react";
 import Topbar from "../../components/topbar/Topbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Feed from "../../components/feed/Feed";
 import Rightbar from "../../components/rightbar/Rightbar";
 import "./home.css"
 
+
 export default function Home() {
+  const [datas, setDatas] = useState();
+  const urlLookup = "http://btax.mandakh.org:8000/lookup/";
+  useEffect(() => {
+    const bodyChiglel = {
+      action: "chiglel",
+    };
+    sendRequest(urlLookup, bodyChiglel);
+  }, []);
+
+  const sendRequest = async (url, body) => {
+    await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(body),
+    })
+      .then((res) => res.json())
+      .then(async (response) => {
+        setDatas(response);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
   return (
     <>
+    <text>
+    {JSON.stringify(datas)}</text>
      <Topbar/>
      <div className="homeContainer">
      <Sidebar/>
